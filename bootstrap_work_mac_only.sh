962c54e37ba1b1753a042e74c0041d8d325ed057
Bobby Wang <bobbywang000@gmail.com>
2023-11-21 15:51:41 -0800

    Repurpose site to contain laptop bootstrapping scripts (#1)
    * tmp
    
    * index page w/ copied links

---
+++ bootstrap_work_mac_only.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run after bootstrap_base_mac.sh
+
+set -euxo pipefail
+
+# Work only casks
+brew install --cask \
+    1password \
+    aws-vpn-client \
+    pgadmin4 \
+    pop \
+    slack \ 
+    zoom
