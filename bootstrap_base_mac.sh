ae13ee31e9489631d9418b1e8d7787be973ff278
Bobby Wang <bobbywang000@gmail.com>
2024-07-29 11:45:53 -0700

    Update bootstrap_base_mac.sh

--- bootstrap_base_mac.sh
+++ bootstrap_base_mac.sh
@@ -60,3 +60,6 @@ rm -rf fonts
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Prevent accidental two finger backswipe
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
